c6fb7bb3492aa515c485e8c3c78b79d75f2a28f7
jfs-coder <jfs-coder@protonmail.com>
2020-11-13 04:33:57 +0000

    adding variables lesson

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int i = 3;
+	float f = 4.5;
+	double d = 5.25;
+	float sum;
+
+	sum = i + f + d;
+
+	printf("The sum of a, b, and c is %f.\n", sum);
+
+	return 0;
+}
