8cb16956a25aeeda0cf0e6d6ffa007ab525b2537
jfs-coder <jfs-coder@protonmail.com>
2020-11-24 17:17:18 +0000

    a few changes in variable names

--- arrayptrs2.c
+++ arrayptrs2.c
@@ -2,18 +2,18 @@
 
 int main() {
 
-	int what[] = {1, 2, 3, 4};
-	int *ptr   = &what[0];
+	int arr[] = {1, 2, 3, 4};
+	int *ptr   = &arr[0];
 
 	printf("*ptr: %d\n", *ptr);
-	printf("what[0]: %d\n", what[0]);
+	printf("arr[0]: %d\n", arr[0]);
+	printf("*(arr): %d\n", *(arr));
 	printf("ptr: %u\n", ptr);
-	printf("&what[0]: %u\n", &what[0]);
+	printf("&arr[0]: %u\n", &arr[0]);
 
 	printf("*ptr + 1: %d\n", *ptr + 1);
-	printf("what + 1: %u\n", what + 1);
-	printf("*(what + 1): %d\n", *(what + 1));
-
-
+	printf("arr + 1: %u\n", arr + 1);
+	printf("*(arr + 1): %d\n", *(arr + 1));
+	
 	return 0;
 }
