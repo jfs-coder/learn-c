7f4dadeae9c873edef5a3983f3e0b0a44bcce512
jfs-coder <jfs-coder@protonmail.com>
2020-12-11 03:48:51 +0000

    added main loop for <print> and <exit> commands and entering numbers to add to list

--- linklist.c
+++ linklist.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct sPerson {
 	int age;
@@ -9,9 +10,14 @@ struct sPerson {
 struct sPerson* getNewPerson(const int age) {
 	struct sPerson* newPerson = NULL;
 	newPerson = malloc(sizeof(struct sPerson));
-	newPerson->nextInLine = NULL;
-	newPerson->age = age;
-	printf("Created new person at %p\n", newPerson);
+	if (newPerson != NULL) {
+		newPerson->nextInLine = NULL;
+		newPerson->age = age;
+		printf("Created new person at %p\n", newPerson);
+	}
+	else {
+		printf("Memory Allocation Failed.\n");
+	}
 	return newPerson;
 }
 
@@ -28,34 +34,72 @@ void printPerson(const struct sPerson* person, const char* comment) {
 	}
 }
 
+void printList(const struct sPerson* list) {
+	printf("Printing List:\n");
+	const struct sPerson* t;
+	t = list;
+	if(t == NULL) {
+		printf("list is empty\n");
+	}
+	else {
+		while(t) {
+			printPerson(t, "t");
+			t = t->nextInLine;
+		}
+	}
+}
+
+void cleanUp(struct sPerson *list) {
+	struct sPerson* next;
+	while(list) {
+		next = list->nextInLine;
+		printf("Cleaning up %d\n", list->age);
+		free(list);
+		list = next;
+	}
+}
+
 int main() {
 	printf("\n\n[START]\n\n");
 
 	struct sPerson* first = NULL;
-	struct sPerson* second = NULL;
+	struct sPerson* added = NULL;
 
-	printPerson(first, " first");
-	printPerson(second,"second");
+	char command[64];
+	int age;
+	// main loop
+	while(1) {
+		printf("Enter a command (exit or print) or value (number): ");
+		fgets(command, 64, stdin);
+		if(!strcmp("exit\n", command)) {
+			printf("Exiting...\n");
+			break;
+		}
+		else if (!strcmp("print\n", command)) {
+			// printf("Printing...\n");
+			printList(first);
+		}
+		else if(sscanf(command, "%d", &age)) {
+			printf("Adding %d\n", age);
+			if(first == NULL) {
+				first = getNewPerson(age);
+				if(first != NULL) {
+					added = first;
+				}
+			}
+			else {
+				added->nextInLine = getNewPerson(age);
+				if(added->nextInLine != NULL) {
+					added = added->nextInLine;
+				}
+			}
+		}
+	}
 
-	first  = getNewPerson(11);
-	second = getNewPerson(19);
+	cleanUp(first);
 
-	printPerson(first, " first");
-	printPerson(second, "second");
-
-	first->nextInLine = second;
-	printPerson(first, "first");
-	printPerson(first->nextInLine, "first->nextInLine");
-	printPerson(second, "second");
-	// second->previousInLine = first;
-
-	printf("\n\n");
-
-	free(first);
-	free(second);
-
-	first  = NULL;
-	second = NULL;
+	first = NULL;
+	added = NULL;
 
 	return 0;
 }
